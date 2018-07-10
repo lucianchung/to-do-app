2827bf37fdf555765a8d6649a64ca8bc0a02d076
Lucian Chung <lucianchung@gmail.com>
2018-07-10 13:10:25 -0400

    Make delete button. Can't make it delete.

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 
 function onReady() {
+  let id=0;
   const toDos=[];
   const addToDoForm = document.getElementById('addToDoForm');
 
+  // declare deleteButton
+  let deleteButton='';
+
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
+
+    // 3. increment the id variable
+        let id=1;
+        id++;
+        ++id;
+
     if (!newToDoText.value) {return;}
+
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id.value
     });
     newToDoText.value = '';
 
     renderTheUI();
+
+//test
+console.log(toDos.id);
+
   }
 
   function renderTheUI() {
@@ -23,12 +39,20 @@ function onReady() {
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
+// 4. create a delete button
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = "Delete";
+
       checkbox.type = "checkbox";
 
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+// 4b. append delete button to newLi
+      newLi.appendChild(deleteButton);
+
+
     });
   }
 
@@ -37,8 +61,19 @@ function onReady() {
     createNewToDo();
   });
 
+//4c register event listener to deleteButton
+  deleteButton.addEventListener('click', function () {
+    if (toDo.id === id) {
+      toDoList.removeChild(newLi);
+    }
+    renderTheUI();
+  });
+
+
   renderTheUI();
 }
+
+
 
 window.onload = function() {
   onReady();
